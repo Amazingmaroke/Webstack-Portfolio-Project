e9750c2889d5876b599317d13c9e58546b4314d2
AMAZING-MAROKE <marokemaroke2020@gmail.com>
2023-12-02 11:11:11 +0100

    server updated

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,18 @@
-import express from "express";
+require('dotenv').config();
+const express =require("express")
+const mongoose =require("mongoose");
+const Connet = require('./configs/databasecon');
+
 
 const app = express();
 const port = 3000;
-
+Connet()
 app.get('/', (req, res) => {
   res.send('Welcome to geval branch server!');
 });
-
+mongoose.connection.once("open",()=>{
+  console.log("connected")
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+});
 });

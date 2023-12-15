a731130324d94fa86edeff9bd4633b2e67f20d73
gevalinho <gevalinho1@gmail.com>
2023-12-15 15:03:36 +0100

    user dashboard

--- Front/src/components/user.jsx
+++ Front/src/components/user.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom';
+import Userdasboard from '../components/Userdashboard';
+import Usersidebar from '../components/Usersidebar';
+import Tableview from './sections/Tableview';
+import Viewuseradmin from './sections/Viewusersadmin';
 
-const User = () => {
+
+
+//import logo from './logo.svg';
+
+
+function User() {
   return (
-    <div>
-      <h1>User dashboard</h1>
+    <div className="">
+      
+      <div className="flex  ">
+        <div className="basis-[12%] h-[100vh]">
+          <Usersidebar />
+        </div>
+        <div className="basis-[88%] overflow-scroll h-[100vh]">
+        <Userdasboard />
+        
+        
+        
+          <div>
+            <Outlet></Outlet>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default User
+export default User;
